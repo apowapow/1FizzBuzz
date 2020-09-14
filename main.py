04a24e0cf895e24fb7233d4d4ec4d801151187a1
POWER Alexander <apower@AltranUK.local>
2020-09-14 11:41:21 +0100

    argparse

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
+import argparse
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no_fizz',    action="store_true")
+    parser.add_argument('--no_buzz',    action="store_true")
+    parser.add_argument('--no_bang',    action="store_true")
+    parser.add_argument('--no_bong',    action="store_true")
+    parser.add_argument('--no_fezz',    action="store_true")
+    parser.add_argument('--no_reverse', action="store_true")
+    args = parser.parse_args()
+
     lower = 1
-    max_upper = 1000000
-    upper = int(input("Enter a maximum number [1-{0}]: ".format(max_upper)))
+    upper = int(input("Enter a maximum number: "))
 
-    if lower <= upper <= max_upper:
-        for i in range(lower, upper+1):
-            fizz = i % 3 == 0
-            buzz = i % 5 == 0
-            bang = i % 7 == 0
-            bong = i % 11 == 0
-            fezz = i % 13 == 0
-            reverse = i % 17 == 0
-            output = []
+    for i in range(lower, upper+1):
+        fizz = i % 3 == 0
+        buzz = i % 5 == 0
+        bang = i % 7 == 0
+        bong = i % 11 == 0
+        fezz = i % 13 == 0
+        reverse = i % 17 == 0
+        output = []
 
-            if bong:
-                if fezz:
-                    output.append("Fezz")
-                output.append("Bong")
-            else:
-                if fizz:
-                    output.append("Fizz")
-                if fezz:
-                    output.append("Fezz")
-                if buzz:
-                    output.append("Buzz")
-                if bang:
-                    output.append("Bang")
+        if (not args.no_bong) and bong:
+            if (not args.no_fezz) and fezz:
+                output.append("Fezz")
+            output.append("Bong")
+        else:
+            if (not args.no_fizz) and fizz:
+                output.append("Fizz")
+            if (not args.no_fezz) and fezz:
+                output.append("Fezz")
+            if (not args.no_buzz) and buzz:
+                output.append("Buzz")
+            if (not args.no_bang) and bang:
+                output.append("Bang")
 
-            if reverse:
-                output.reverse()
+        if (not args.no_reverse) and reverse:
+            output.reverse()
 
-            if len(output) > 0:
-                print(''.join(output))
+        if len(output) > 0:
+            print(''.join(output))
+        else:
+            print(i)
 
 
 if __name__ == "__main__":
